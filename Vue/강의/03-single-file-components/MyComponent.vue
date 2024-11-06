9caf6d3f6c9cf608e8a9d3e17db5b728ae3504b6
Hanyikyeong <hanna5244@naver.com>
2024-11-06 17:58:32 +0900

    vue 4차

---
+++ Vue/강의/03-single-file-components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<!-- Vue CDN -->
+<script setup>
+  import {ref} from 'vue'
+  
+  const msg = ref('hello')
+</script>
+
+<style scoped>
+.greeting {
+  color: red;
+}
+</style>
---
+++ Vue/강의/03-single-file-components/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>MyComponent</h2>
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script setup>
+  import MyComponentItem from './MyComponentItem.vue';
+</script>
+
+<style scoped>
+
+</style>
